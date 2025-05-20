e965002520e276ff8857aa9faec33a79701a3e34
Raphaël Chiche <raphael.ch2004@gmail.com>
2025-05-20 11:07:58 +0200

    Adding Delet Btn when admin

--- front/app/page.tsx
+++ front/app/page.tsx
@@ -1,16 +1,26 @@
 "use client";
- 
+
+import { getSession, getToken } from "@/utils/jwt";
 import { Project } from "@/types/project";
 import { useEffect, useState } from "react";
 import Link from "next/link";
+import { isAscii } from "buffer";
 
- 
 export default function Home() {
   const [projects, setProjects] = useState<Project[]>([]);
- 
-  // Récupération des projets
+  const [isAdmin, setIsAdmin] = useState(false);
+  const [response, setResponse] = useState("");
+
+
+  const fetchSession = async () => {
+    const session = await getSession();
+    if (session) {
+      setIsAdmin(session.roles.includes("ROLE_ADMIN"));
+    }
+  };
+
   const getProjects = async () => {
-    try{
+    try {
       const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/projects`);
       const data = await res.json();
       setProjects(data.member as Project[]);
@@ -18,11 +28,34 @@ export default function Home() {
       console.error(error);
     }
   };
- 
+
+  const handleDelete = async (projectId: number) => {
+    const token = await getToken();
+
+    const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/projects/${projectId}`, {
+      method: "DELETE",
+      headers: {
+        "Authorization": `Bearer ${token}`,
+      },
+    });
+
+    if (response.ok) {
+      setResponse("Projet supprimé avec succès");
+      setProjects((prevProjects) => prevProjects.filter((project) => project.id !== projectId));
+    } else {
+      const data = await response.json();
+      console.error(data);
+      setResponse("Échec lors de la suppression du projet");
+    }
+
+  };
+
+
   useEffect(() => {
+    fetchSession();
     getProjects();
   }, []);
- 
+
   return (
     <>
       <h1>Liste des projets</h1>
@@ -36,6 +69,13 @@ export default function Home() {
               <p>
                 <Link href={`/project/${project.id}`}>Afficher le projet</Link>
               </p>
+              {isAdmin && (
+                <button
+                  onClick={() => handleDelete(project.id)}
+                >
+                  Supprimer ce projet
+                </button>
+              )}
               <hr />
             </li>
           ))}
